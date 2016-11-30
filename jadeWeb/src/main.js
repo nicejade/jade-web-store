83da2e44929c8e1383a2755250e45239cb587f03
jeffjade <jeffygisgreat@gmail.com>
2016-12-01 00:30:39 +0800

    update about include elemenetUI

--- jadeWeb/src/main.js
+++ jadeWeb/src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
 import App from './App'
+import locale from 'element-ui/lib/locale/lang/en'
+
+Vue.use(ElementUI, { locale })
 
 /* eslint-disable no-new */
 new Vue({
